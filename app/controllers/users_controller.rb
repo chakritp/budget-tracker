5dadd980a0d5a38272626803a79910e1a527d65a
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-09-26 12:11:27 -0700

    Add validations for name, email and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to Pocket Trackr #{@user.first_name}!"
       redirect_to dashboard_path
     else
-      flash[:danger] = "Please ensure all fields are filled correctly"
+      flash[:danger] = email_taken? ? "This email has already been taken. Please input another email." : "Please ensure all fields are filled correctly"
       redirect_to new_user_path
     end
   end
@@ -32,6 +32,10 @@ class UsersController < ApplicationController
   end
 
   private
+  
+  def email_taken?
+    @user.errors.full_messages.include?("Email has already been taken")
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates :email, uniqueness: true
+
   has_many :payment_methods
   has_many :transactions, through: :payment_methods
+
 end
