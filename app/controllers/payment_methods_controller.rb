93c2b2e454d8cf9c1904d21d0a5e3b421f1a5357
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-09-26 16:03:37 -0700

    Implement create and destroy payment method

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -8,9 +8,20 @@ class PaymentMethodsController < ApplicationController
   end
 
   def new
+    @payment_method = PaymentMethod.new
   end
 
   def create
+    @payment_method = PaymentMethod.new(payment_method_params)
+    @payment_method.user = current_user
+
+    if @payment_method.save
+      flash[:success] = "You have successfully added a new card"
+      redirect_to payment_methods_path
+    else
+      flash[:danger] = "Please ensure the fields are filled in correctly"
+      redirect_to new_payment_method_path
+    end
   end
 
   def edit
@@ -20,5 +31,19 @@ class PaymentMethodsController < ApplicationController
   end
 
   def destroy
+    @payment_method = PaymentMethod.find(params[:id])
+    if @payment_method.destroy
+      flash[:success] = "Card #{@payment_method.bank} #{@payment_method.last_four_digits} has been successfully deleted"
+      redirect_to payment_methods_path
+    else
+      flash[:danger] = "Something went wrong. Please try again."
+      redirect_to payment_methods_path
+    end
+  end
+
+  private
+  
+  def payment_method_params
+    params.require(:payment_method).permit(:payment_type, :bank, :last_four_digits)
   end
 end
