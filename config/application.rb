de45334f2d8081df03eb78ba7f9e1669eeef404e
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-09-28 20:58:11 -0700

    Add paperclip config

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,16 @@ module BudgetTracker
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_region: ENV['AWS_REGION'],
+      s3_credentials: {
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+          :bucket => ENV['S3_BUCKET']
+      },
+      url: ':s3_domain_url',
+      path: '/:class/:attachment/:id_partition/:style/:filename'
+    }
   end
 end
